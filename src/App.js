3ecb9a85d474a8168015488ed64614e8fdb93999
Felix-fale <falefelix23@gmail.com>
2022-12-01 06:42:34 +0000

    add form (done)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  // State (etat, données)
+  const [fruits, setFruits] = useState([
+    { id: 1, nom: "Abricot" },
+    { id: 2, nom: "Banane" },
+    { id: 3, nom: "Cerise" },
+  ]);
+
+  const [nouveauFruit, setNouveauFruit] = useState("");
+
+  // comportements
+  const handleDelete = (id) => {
+    console.log(id);
+    // 1.copie du state
+    const fruitsCopy = [...fruits];
+
+    // 2.Manipuler le state
+    const fruitsCopyUpdated = fruitsCopy.filter((fruit) => fruit.id !== id);
+
+    // 3.Modifier le state avec le setter
+    setFruits(fruitsCopyUpdated);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // 1 copie du state
+    const fruitsCopy = [...fruits];
+    // 2 manipulation sur le copie du state
+    const id = new Date().getTime();
+    const nom = nouveauFruit;
+    fruitsCopy.push({ id, nom });
+    // 3 modifier le state avec le setter
+    setFruits(fruitsCopy);
+    setNouveauFruit("");
+  };
+
+  const handleChange = (event) => {
+    setNouveauFruit(event.target.value);
+  };
+
+  // affichage (render)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Liste de fruits</h1>
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit.id}>
+            {fruit.nom}{" "}
+            <button onClick={() => handleDelete(fruit.id)}>X</button>
+          </li>
+        ))}
+      </ul>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          value={nouveauFruit}
+          type="text"
+          placeholder="Ajouter un fruit..."
+          onChange={handleChange}
+        />
+        <button>Ajouter + 58m30s</button>
+      </form>
     </div>
   );
 }
